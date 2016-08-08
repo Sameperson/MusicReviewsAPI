3028d30476c1d4c8cc81fc08704ad346a5c0d231
Saem <ottosigma@gmail.com>
2016-08-08 16:24:22 +0300

    Populated database

--- src/main/java/com/sameperson/core/DatabaseLoader.java
+++ src/main/java/com/sameperson/core/DatabaseLoader.java
@@ -8,6 +8,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 @Component
 public class DatabaseLoader implements ApplicationRunner {
 
@@ -23,6 +27,30 @@ public class DatabaseLoader implements ApplicationRunner {
         Composition composition = new Composition("TheFatRat - Monody (feat. Laura Brehm)", "https://www.youtube.com/watch?v=nw5Mc5bpq-A");
         composition.addReview(new Review(5, "Great track, nice production and mixing!"));
         compositions.save(composition);
+
+        String[] templates = {
+            "TheFatRat - Song about %s",
+            "Studio Killers - %s song",
+            "Caravan Palace - Rise of the %s",
+            "Carpenter Brut - Dance of %s"
+        };
+        String[] words = {
+            "Robots",
+            "Mad people",
+            "DJ",
+            "AI",
+            "Aliens"
+        };
+        List<Composition> collectionOfCompositions = new ArrayList<>();
+        IntStream.range(0, 100).forEach(i -> {
+            String template = templates[i % templates.length];
+            String word = words[i % words.length];
+            String title = String.format(template, word);
+            Composition c = new Composition(title, "https://www.google.com.ua/");
+            c.addReview(new Review(i % 5, title + " is rad stuff!"));
+            collectionOfCompositions.add(c);
+        });
+        compositions.save(collectionOfCompositions);
 
     }
 }
