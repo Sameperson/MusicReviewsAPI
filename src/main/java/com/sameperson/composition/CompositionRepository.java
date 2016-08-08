8db49efb6893a247a47bc7024fe7ee78ac68f4c3
Saem <ottosigma@gmail.com>
2016-08-08 17:16:54 +0300

    Added paging and search

--- src/main/java/com/sameperson/composition/CompositionRepository.java
+++ src/main/java/com/sameperson/composition/CompositionRepository.java
@@ -1,6 +1,14 @@
 package com.sameperson.composition;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface CompositionRepository extends CrudRepository<Composition, Long> {
+public interface CompositionRepository extends PagingAndSortingRepository<Composition, Long> {
+
+    @RestResource(path = "containsTitle", rel = "title-contains")
+    Page<Composition> findByTitleContaining(@Param("title") String title, Pageable page);
+
 }
